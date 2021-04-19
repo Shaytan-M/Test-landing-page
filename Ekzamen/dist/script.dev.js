d913fae5bc4e42531fd6fb3f282c21c882c8c719
Misha <73448452+Shaytan-M@users.noreply.github.com>
2021-04-19 13:05:54 +0300

    Ekzamen

---
+++ Ekzamen/dist/script.dev.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var menu_btn = document.querySelector('#burger'),
+    navigate = document.querySelector('.mobile-menu'),
+    body = document.querySelector('body');
+menu_btn.addEventListener('click', function () {
+  navigate.classList.toggle('menu-active');
+  this.classList.toggle('burger-active');
+  body.classList.toggle('no-scroll');
+});
+window.addEventListener('scroll', function () {
+  navigate.classList.remove('menu-active');
+  menu_btn.classList.remove('burger-active');
+  body.classList.remove('no-scroll');
+});
